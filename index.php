3104f405d1d6fec14619e12059f24672d92840e9
kdarshan425 <64018424+kdarshan425@users.noreply.github.com>
2021-08-21 21:29:53 +0530

    update

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< Updated upstream
     <link rel="icon" href="img/topl.png">
+=======
+>>>>>>> Stashed changes
     <title>Cropit | Home</title>
     <link rel="stylesheet" type="text/css" href="styles/header.css">
     <link rel="stylesheet" type="text/css" href="styles/index.css">
