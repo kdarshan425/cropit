168beebeb66e8f5ff2559d86af6ba67ac8034ae6
kdarshan425 <64018424+kdarshan425@users.noreply.github.com>
2021-08-21 21:50:48 +0530

    database updated

--- partials/dbconnect.php
+++ partials/dbconnect.php
@@ -1,9 +1,9 @@
 <?php 
 
 $servername = "remotemysql.com";
-$username = "hjtIutuWyg";
-$password = "1b7eTjbNdd";
-$database = "hjtIutuWyg";
+$username = "XmWxTAQrDK";
+$password = "Coq13b3z1K";
+$database = "XmWxTAQrDK";
 
 $conn = mysqli_connect($servername, $username, $password, $database) or die(mysqli_error($con));
 
